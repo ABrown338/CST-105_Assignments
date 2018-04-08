19e4e7916e1e207363d797389a4bddc2e6a18777
AlexBrown <ABrown338@my.gcu.edu>
2018-04-08 13:52:54 -0600

    Step 2 and 3 of Programming Exercise 1

---
+++ Programming Exercise 1/HelloWorld.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author Alex Brown
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("My name is Alex Brown");
+        System.out.println("My favorite color is red");
+        System.out.println("If I won the Lottery I would invest the money");
+    }
+    
+}
